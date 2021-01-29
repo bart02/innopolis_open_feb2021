4675a48d24e9f79f61f9de5908b00578fd55ef3c
Artem Batalov <a@batalov.me>
2021-01-30 00:09:09 +0300

    Add photos

--- hidden_innoopen/update.py
+++ hidden_innoopen/update.py
@@ -5,14 +5,33 @@ import qrcode
 
 fn = '/home/clover/catkin_ws/src/clover/clover_simulation/resources/worlds/clover.world'
 qr_fn = '/home/clover/.gazebo/models/aruco_map_txt/materials/textures/aruco_marker_2_90.png'
+face_fn = '/home/clover/.gazebo/models/{0}/{0}.sdf'
 
-rnd_permutation = lambda x: choice(list(permutations(x)))
+rnd_permutation = lambda x: choice(list(permutations(x,3)))
 pose = lambda x_mix, x_max, y_min, y_max: '      <pose>{} {} 0 0 0 0</pose>\n'.format(randint(x_mix, x_max) / 10.0, randint(y_min, y_max) / 10.0)
 
 
 # QR
 qrs = ['yellow','blue','red']
 qrcode.make(','.join(rnd_permutation(qrs))).save(qr_fn)
+
+
+# Photos
+names = ["              <name>faces/sherlock</name>\n",
+"              <name>faces/jack</name>\n",
+"              <name>faces/einstein</name>\n",
+"              <name>faces/silverhand</name>\n"]
+name = rnd_permutation(names)
+
+for i, dp in enumerate(('dronepoint_blue', 'dronepoint_red', 'dronepoint_yellow')):
+    with open(face_fn.format(dp), 'r') as file:
+        data = file.readlines()
+    
+    data[32] = name[i]
+
+    with open(face_fn.format(dp), 'w') as file:
+        file.writelines( data )
+
 
 # Dronepoints
 dps = ["      <uri>model://dronepoint_blue</uri>\n",
