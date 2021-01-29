0fcc34f5469fc70a6174058d5380ac5c64ebc5e5
Artem Batalov <a@batalov.me>
2021-01-30 00:08:39 +0300

    Update script without photos
    hotfix

--- hidden_innoopen/update.py
+++ hidden_innoopen/update.py
@@ -3,33 +3,32 @@ from itertools import permutations
 from random import choice, randint
 import qrcode
 
-
 fn = '/home/clover/catkin_ws/src/clover/clover_simulation/resources/worlds/clover.world'
 qr_fn = '/home/clover/.gazebo/models/aruco_map_txt/materials/textures/aruco_marker_2_90.png'
 
 rnd_permutation = lambda x: choice(list(permutations(x)))
+pose = lambda x_mix, x_max, y_min, y_max: '      <pose>{} {} 0 0 0 0</pose>\n'.format(randint(x_mix, x_max) / 10.0, randint(y_min, y_max) / 10.0)
+
 
 # QR
-qrs = ['yellow','blue','green']
-qrcode.make(','.join(rnd_permutation(qrs)) + ',red').save(qr_fn)
+qrs = ['yellow','blue','red']
+qrcode.make(','.join(rnd_permutation(qrs))).save(qr_fn)
 
 # Dronepoints
 dps = ["      <uri>model://dronepoint_blue</uri>\n",
-"      <uri>model://dronepoint_green</uri>\n",
+"      <uri>model://dronepoint_red</uri>\n",
 "      <uri>model://dronepoint_yellow</uri>\n"]
 dp = rnd_permutation(dps)
 
-x = randint(52, 88) / 10.0
-y = randint(52, 88) / 10.0
-dp_4 = '      <pose>{} {} 0 0 0 0</pose>\n'.format(x,y)
+# 0.2-2.6, 5.4-8.6
+# 5.2-7.6, 5.4-8.6
+# 5.2-7.6, 0.4-3.6
 
 with open(fn, 'r') as file:
     data = file.readlines()
 
-data[19] = dp[0]
-data[24] = dp[1]
-data[29] = dp[2]
-data[36] = dp_4
+data[19], data[24], data[29] = dp
+data[21], data[26], data[31] = pose(2,26,54,86), pose(52,76,54,86), pose(52,76,4,36)
 
 with open(fn, 'w') as file:
     file.writelines( data )
